ba4990c30afaabb8ab40bbb906a4eae508ebb57f
Mete Yildiz <mete.yildiz@smail.th-koeln.de>
2020-08-13 10:38:48 +0200

    lvl und modus getter und setter sind drin

--- app/src/main/java/com/example/quizappv2/ChooseModusActivity.kt
+++ app/src/main/java/com/example/quizappv2/ChooseModusActivity.kt
@@ -6,21 +6,34 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ChooseModusActivity : AppCompatActivity() {
+
+    var modus = "";
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modus)
 
         val testModusBtn = findViewById<Button>(R.id.Testmodusbtn)
         testModusBtn.setOnClickListener{
+            setmodus("test")
             val intent = Intent(this, WissenLevelAcitivity::class.java)
             startActivity(intent)
         }
 
         val lernModusBtn = findViewById<Button>(R.id.Lernmodusbtn)
         lernModusBtn.setOnClickListener{
+            setmodus("lern")
             val intent = Intent(this, WissenLevelAcitivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun setmodus(modi : String){
+        this.modus = modi
+    }
+
+    public fun getmodus() : String{
+        return modus
     }
 
 }
--- app/src/main/java/com/example/quizappv2/WissenLevelAcitivity.kt
+++ app/src/main/java/com/example/quizappv2/WissenLevelAcitivity.kt
@@ -8,20 +8,32 @@ import com.example.quizappv2.R
 
 class WissenLevelAcitivity : AppCompatActivity() {
 
+    var lvl = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wissen_level)
 
         val grundWissenBtn = findViewById<Button>(R.id.GrundwissenBtn)
         grundWissenBtn.setOnClickListener{
+            setlvl(1)
             val intent = Intent(this, StartTestActivity::class.java)
             startActivity(intent)
         }
 
         val erweitertesWissenBtn = findViewById<Button>(R.id.ErweitertesWissenBtn)
         erweitertesWissenBtn.setOnClickListener{
+            setlvl(2)
             val intent = Intent(this, StartTestActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun setlvl(i : Int){
+        this.lvl = i
+    }
+
+    public fun getlvl() : Int{
+        return this.lvl
     }
 }
